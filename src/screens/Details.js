94355b1132fc140ac1fc7ae9631dbbe4be787350
seyda.taskindavut <seyda.taskindavut@trendyol.com>
2022-04-22 09:45:04 +0300

    Add favorites

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,16 +1,48 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useReducer } from "react";
 import { DetailCard, Button } from "../components";
 import { responsive } from "../utils/responsive";
 import HeartIcon from "../assets/icons/heart.svg";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { combineReducers } from "../utils/combineReducer";
+
+const initialState = {
+  favoriteList : [],
+};
 
 export const Details = ({ route }) => {
+  const { item = {} } = route?.params;
+  const [state, setState] = useReducer(combineReducers, initialState);
+
+  const { favoriteList = [] } = state || {};
+
+  const isFavorite = (item) => {
+
+    const itemIndex = favoriteList.findIndex((favorite) => favorite.id === item.id);
+    return itemIndex > -1;
+  }
+
+  const addFavorite = async() => {
+    if (isFavorite(item)) {
+      const newList = favoriteList.filter((favorite) => favorite.id !== item.id );
+      
+      await AsyncStorage.setItem('favorites', JSON.stringify(newList));
+      
+      setState({ favoriteList: newList });
+    } else {
+      const newList = [...favoriteList, item];
+      await AsyncStorage.setItem('favorites', JSON.stringify(newList));
+
+      setState({ favoriteList: newList });
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <DetailCard item={route.params.item} />
+      <DetailCard item={item} />
       <Button
-        title="Add to Favorites"
-        onPress={() => console.log("pressed")}
+        title={isFavorite(item) ? 'Added': 'Add to Favorite'}
+        onPress={addFavorite}
         Icon={HeartIcon}
       />
     </View>
--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,12 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useCallback } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const Favorites = () => {
+
+  useFocusEffect(
+    useCallback(() => {
+      const getFavorites = async() => {
+        const list = await AsyncStorage.getItem('favorites');
+      };
+      getFavorites();
+    }, []);
+  );
+
   return (
     <View>
       <Text>Favorites</Text>
     </View>
   )
-}
+};
 
 const styles = StyleSheet.create({})
